2491a7cf822ff8ce7ee76f54c69b1c6cdb977e9b
wdfwwkkee <denis.usmanov101@mail.ru>
2023-10-11 17:04:38 +0500

    desc

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".heroSwiper", {
+    spaceBetween: 30,
+    simulateTouch: false,
+    centeredSlides: true,
+    effect: "fade",
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+  });
